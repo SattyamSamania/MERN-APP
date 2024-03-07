2bafd132b4856c9fab0dc651c1db2e84e6a9b2c4
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-03-07 15:02:26 +0530

    Updated Workout Details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Navbar from '../components/Navbar';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,34 +1,34 @@
-import React, {useEffect, useState} from 'react'
-import WorkoutDetails from '../../components/WorkoutDetails';
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
-
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext()
 
-const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('http://localhost:4000/api/workouts')
+      const json = await response.json()
 
-useEffect(() => {
-  const fetchWorkouts = async () => {
-
-    const response = await fetch('http://localhost:4000/api/workouts')
-    const json = await  response.json();
-
-    if(response.ok){
-        setWorkouts(json); 
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
     }
-  }
 
- fetchWorkouts()
-}, [])
-
+    fetchWorkouts()
+  }, [dispatch])
 
   return (
-    <div className='home'>
-       <div className='workouts'>
-        {workouts && workouts.map((workout) =>  (
-           <WorkoutDetails  key={workout._id}  workout={workout}/>
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
         ))}
-       </div>
+      </div>
+      <WorkoutForm />
     </div>
   )
 }
